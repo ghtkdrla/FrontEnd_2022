5dd123f87bb4400e68c33ec94618315c2a484d5c
ghtkdrla <ghtkdrla321@naver.com>
2022-05-04 00:50:03 +0900

    3주차 과제 제출

--- 멋쟁이사자처럼 전화번호부.py
+++ 멋쟁이사자처럼 전화번호부.py
@@ -1,9 +1,27 @@
+import requests
+import json
+
+city = "Seoul"
+apikey = "81e8e9a373613dd8c2d9dc978073aee0"
+lang = "kr"
+
+api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}&lang={lang}&units=metric"
+
+result = requests.get(api)
+data = json.loads(result.text)
+
+print(data["name"],"의 날씨입니다.")
+print("날씨는 ",data["weather"][0]["description"],"입니다.")
+print("현재 온도는 ",data["main"]["temp"],"입니다.")
+print("하지만 체감 온도는 ",data["main"]["feels_like"],"입니다.")
+
+
 user_info = []
 
 
 def picker():
     print("------------- 멋쟁이 사자처럼 전화번호부 -------------")
-    print("-----1" + ") 추가 2" + ") 조회 3" + ") 수정 4" + ") 삭제 q" +") 종료 -----")
+    print("-----1" + ") 추가 2" + ") 조회 3" + ") 수정 4" + ") 삭제 5" + ") 메일 전송 q" +") 종료 -----")
     print("---------------------------------------------------------------")
     num = input("원하시는 메뉴를 입력해주세요: ")
     if num == "1":
@@ -17,6 +35,8 @@ def picker():
         exit()
     elif num == "4":
         delete_user()
+    elif num == "5":
+        mailing()
     else:
         print("wrong input")
         picker()
@@ -83,6 +103,9 @@ def delete_user():
             del i['메일']
             
     picker()
+    
+def mailing():
+    mail_name = input("메일 전송을 원하는 사람의 이름을 입력해주세요 : ")
     
 picker()
     
